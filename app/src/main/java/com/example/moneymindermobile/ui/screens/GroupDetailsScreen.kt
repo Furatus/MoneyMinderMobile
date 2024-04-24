ffb4e9df5321d3af8c5c905a4dadb0e30e92bc5d
Furatus <furacoptere@gmail.com>
2024-04-24 22:23:31 +0200

    Added AddExppenseButton will do sheet tomorrow

--- app/src/main/java/com/example/moneymindermobile/ui/screens/GroupDetailsScreen.kt
+++ app/src/main/java/com/example/moneymindermobile/ui/screens/GroupDetailsScreen.kt
@@ -1,19 +1,23 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 
 package com.example.moneymindermobile.ui.screens
 
-import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
@@ -53,7 +57,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
@@ -63,7 +66,6 @@ import com.example.moneymindermobile.Routes
 import com.example.moneymindermobile.data.MainViewModel
 import com.example.moneymindermobile.data.api.ApiEndpoints
 import com.example.moneymindermobile.ui.components.EntityImage
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
@@ -350,10 +352,19 @@ fun GroupDetailsScreen(
                     if (isGroupByIdCreatedByCurrentUser) Text(text = "You are the owner of this group")
 
                     var selectedTabIndex by rememberSaveable { mutableIntStateOf(0) }
+                    val pagerState = rememberPagerState(pageCount =  {tabItems.size})
+
+                    LaunchedEffect(pagerState.currentPage) {
+
+                        selectedTabIndex = pagerState.currentPage
+                    }
 
                     TabRow(selectedTabIndex = selectedTabIndex) {
                         tabItems.forEachIndexed { index, item ->
                             Tab(selected = index == selectedTabIndex, onClick = {
+                                scope.launch {
+                                    pagerState.animateScrollToPage(index)
+                                }
                                 selectedTabIndex = index
                             },
                                 text = {
@@ -367,11 +378,31 @@ fun GroupDetailsScreen(
                                 })
                         }
                     }
+                    
+                    HorizontalPager(state = pagerState, modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .weight(1f)) { index ->
+                        run {
+                            if (index == 0) {
+                                Box(modifier = Modifier.fillMaxWidth().fillMaxHeight().padding(5.dp)) {
+                                    Button(onClick = { /*TODO*/ }, modifier = Modifier.size(50.dp).align(Alignment.BottomCenter), contentPadding = PaddingValues(1.dp)) {
+                                        Icon(imageVector = Icons.Filled.Add, contentDescription = "Add Expense")
+                                    }
+                                }
+                            }
+                            if (index == 1) {
+                                Text(text = "I'm another custom text")
+                            }
+                        }
+
+                    }
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun LazyRowOfUsersToInvite(

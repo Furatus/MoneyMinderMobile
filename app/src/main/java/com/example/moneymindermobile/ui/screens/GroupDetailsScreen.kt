66a48472f0b5e34f6fdfa47225daeeb82a5df138
Furatus <furacoptere@gmail.com>
2024-04-24 20:42:36 +0200

    Added Tabs in the GroupDetailsScreen

--- app/src/main/java/com/example/moneymindermobile/ui/screens/GroupDetailsScreen.kt
+++ app/src/main/java/com/example/moneymindermobile/ui/screens/GroupDetailsScreen.kt
@@ -18,10 +18,14 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.ArrowForward
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.ExitToApp
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
@@ -29,6 +33,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -37,12 +43,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -60,9 +68,7 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun GroupDetailsScreen(
-    groupId: String?,
-    viewModel: MainViewModel,
-    navController: NavHostController
+    groupId: String?, viewModel: MainViewModel, navController: NavHostController
 ) {
     val currentUserId =
         viewModel.currentUserResponse.collectAsState().value?.currentUser?.id?.toString()
@@ -70,7 +76,7 @@ fun GroupDetailsScreen(
     val isGetUsersByUsernameLoading = viewModel.isGetUsersByUsernameLoading.collectAsState()
     val firstFetchDone by rememberSaveable { mutableStateOf(false) }
     val currentGroupByIdState = viewModel.groupByIdResponse.collectAsState()
-    val refreshTrigger = rememberSaveable { mutableStateOf(0)    }
+    val refreshTrigger = rememberSaveable { mutableStateOf(0) }
 
     val sheetStateOptions = rememberModalBottomSheetState()
     var addUser by rememberSaveable { mutableStateOf(false) }
@@ -83,23 +89,33 @@ fun GroupDetailsScreen(
     val getUsersByUsernameResponse = viewModel.getUsersByUsernameResponse.collectAsState()
 
     val scope = rememberCoroutineScope()
-    val GroupNameTextField  = rememberSaveable {  mutableStateOf("default name")  }
-    val GroupDescriptionTextField = rememberSaveable { mutableStateOf("default description")}
+    val GroupNameTextField = rememberSaveable { mutableStateOf("default name") }
+    val GroupDescriptionTextField = rememberSaveable { mutableStateOf("default description") }
+
+    val tabItems = listOf(
+        TabItem(
+            title = "Expenses",
+            unselectedIcon = Icons.Outlined.ExitToApp,
+            selectedIcon = Icons.Filled.ExitToApp
+        ), TabItem(
+            title = "Balance",
+            unselectedIcon = Icons.Outlined.ArrowForward,
+            selectedIcon = Icons.Filled.ArrowForward
+        )
+    )
 
     LaunchedEffect(getUsersByUsernameTextField.value) {
         viewModel.getUsersByUsername(getUsersByUsernameTextField.value)
     }
 
     LaunchedEffect(groupId, refreshTrigger.value) {
-        if (groupId != null)
-            viewModel.getGroupById(groupId)
+        if (groupId != null) viewModel.getGroupById(groupId)
     }
 
-    Column (modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
         if (isLoading.value) {
             Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
+                contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()
             ) {
                 CircularProgressIndicator()
             }
@@ -113,7 +129,8 @@ fun GroupDetailsScreen(
                 if (isOptionsSheetOpen) {
                     ModalBottomSheet(
                         onDismissRequest = { isOptionsSheetOpen = false },
-                        sheetState = sheetStateOptions) {
+                        sheetState = sheetStateOptions
+                    ) {
                         TextField(
                             value = GroupNameTextField.value,
                             leadingIcon = {
@@ -144,7 +161,10 @@ fun GroupDetailsScreen(
                                 .fillMaxWidth()
                                 .padding(8.dp),
                         )
-                        Button(onClick = { scope.launch { sheetStateOptions.hide()}.invokeOnCompletion { isOptionsSheetOpen = false } }) {
+                        Button(onClick = {
+                            scope.launch { sheetStateOptions.hide() }
+                                .invokeOnCompletion { isOptionsSheetOpen = false }
+                        }) {
                             Text(text = "Submit changes")
                         }
                         Spacer(modifier = Modifier.padding(30.dp))
@@ -153,11 +173,15 @@ fun GroupDetailsScreen(
 
                 if (isMembersSheetOpen) {
                     ModalBottomSheet(
-                        onDismissRequest = { isMembersSheetOpen = false },
-                        sheetState = sheetStateMembers) {
+                        onDismissRequest = {
+                            isMembersSheetOpen = false
+                            addUser = false
+                        }, sheetState = sheetStateMembers
+                    ) {
                         if (!addUser) {
                             Box(
-                                contentAlignment = Alignment.Center, modifier = Modifier
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier
                                     .fillMaxWidth()
                                     .padding(5.dp)
                             ) {
@@ -166,7 +190,11 @@ fun GroupDetailsScreen(
                                         text = "Members of ${groupById.name}",
                                         modifier = Modifier.padding(bottom = 15.dp)
                                     )
-                                    Card(modifier = Modifier.fillMaxWidth().padding(top = 8.dp)) {
+                                    Card(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(top = 8.dp)
+                                    ) {
                                         val currentGroupMembersWithoutCurrentUser =
                                             groupById.userGroups.filter { it.user.id != currentUserId }
                                         if (currentGroupMembersWithoutCurrentUser.isEmpty()) {
@@ -191,7 +219,8 @@ fun GroupDetailsScreen(
                                     }
                                 }
                                 IconButton(
-                                    onClick = { addUser = true }, modifier = Modifier
+                                    onClick = { addUser = true },
+                                    modifier = Modifier
                                         .align(Alignment.TopEnd)
                                         .size(50.dp)
                                 ) {
@@ -201,14 +230,17 @@ fun GroupDetailsScreen(
                                     )
                                 }
                             }
-                        }
-                        else {
+                        } else {
                             Box(
-                                contentAlignment = Alignment.Center, modifier = Modifier
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier
                                     .fillMaxWidth()
                                     .padding(5.dp)
                             ) {
-                                Column (modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                                Column(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
 
                                     Text(
                                         text = "Add member to ${groupById.name}",
@@ -251,7 +283,8 @@ fun GroupDetailsScreen(
                                     }
                                 }
                                 IconButton(
-                                    onClick = { addUser = false }, modifier = Modifier
+                                    onClick = { addUser = false },
+                                    modifier = Modifier
                                         .align(Alignment.TopEnd)
                                         .size(50.dp)
                                 ) {
@@ -271,14 +304,13 @@ fun GroupDetailsScreen(
                     Button(onClick = { isOptionsSheetOpen = true }) {
                         Row(verticalAlignment = Alignment.CenterVertically) {
                             Icon(
-                                imageVector = Icons.Outlined.Edit,
-                                contentDescription = "Edit icon"
+                                imageVector = Icons.Outlined.Edit, contentDescription = "Edit icon"
                             )
                             Spacer(modifier = Modifier.padding(5.dp))
                             Text(text = "Edit group")
                         }
                     }
-
+                    Spacer(modifier = Modifier.padding(5.dp))
                     Button(onClick = { isMembersSheetOpen = true }) {
                         Row(verticalAlignment = Alignment.CenterVertically) {
                             Icon(
@@ -301,25 +333,40 @@ fun GroupDetailsScreen(
                             Icon(
                                 imageVector = Icons.Filled.AccountBox,
                                 contentDescription = "${groupById.name} default image",
-                                modifier = Modifier
-                                    .size(64.dp)
+                                modifier = Modifier.size(64.dp)
                             )
                         } else {
                             AsyncImage(
                                 model = groupById.groupImageUrl.replace(
-                                    "localhost",
-                                    ApiEndpoints.API_ADDRESS
+                                    "localhost", ApiEndpoints.API_ADDRESS
                                 ),
                                 contentDescription = "${groupById.name} avatar",
                                 contentScale = ContentScale.Crop,
-                                modifier = Modifier
-                                    .size(64.dp)
+                                modifier = Modifier.size(64.dp)
                             )
                         }
                         Text(text = groupById.name)
                     }
-                    if (isGroupByIdCreatedByCurrentUser)
-                        Text(text = "You are the owner of this group")
+                    if (isGroupByIdCreatedByCurrentUser) Text(text = "You are the owner of this group")
+
+                    var selectedTabIndex by rememberSaveable { mutableIntStateOf(0) }
+
+                    TabRow(selectedTabIndex = selectedTabIndex) {
+                        tabItems.forEachIndexed { index, item ->
+                            Tab(selected = index == selectedTabIndex, onClick = {
+                                selectedTabIndex = index
+                            },
+                                text = {
+                                    Text(text = item.title)
+                                },
+                                icon = {
+                                    Icon(
+                                        imageVector = if (index == selectedTabIndex) item.selectedIcon else item.unselectedIcon,
+                                        contentDescription = "${item.title} Icon"
+                                    )
+                                })
+                        }
+                    }
                 }
             }
         }
@@ -333,12 +380,12 @@ fun LazyRowOfUsersToInvite(
     invited: List<GetGroupByIdQuery.Invitation>,
     refreshTrigger: MutableState<Int>,
     onMemberClicked: (GetUsersByUsernameQuery.User) -> Unit
-){
+) {
     val memberIds = members.map { it.user.id }
     val invitedIds = invited.map { it.user.id }
     val usersToInvite = users.filter { it.id !in memberIds && it.id !in invitedIds }
     LazyRow {
-        items(usersToInvite) {user ->
+        items(usersToInvite) { user ->
             UserToInviteCard(toInvite = user, onMemberClicked = onMemberClicked)
         }
     }
@@ -359,14 +406,11 @@ fun LazyRowOfMembers(
 
 @Composable
 fun UserToInviteCard(
-    toInvite: GetUsersByUsernameQuery.User,
-    onMemberClicked: (GetUsersByUsernameQuery.User) -> Unit
+    toInvite: GetUsersByUsernameQuery.User, onMemberClicked: (GetUsersByUsernameQuery.User) -> Unit
 ) {
-    Card(
-        modifier = Modifier
-            .padding(8.dp)
-            .clickable { onMemberClicked(toInvite) }
-    ) {
+    Card(modifier = Modifier
+        .padding(8.dp)
+        .clickable { onMemberClicked(toInvite) }) {
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -378,14 +422,11 @@ fun UserToInviteCard(
 
 @Composable
 fun GroupMemberCard(
-    member: GetGroupByIdQuery.UserGroup,
-    onMemberClicked: (GetGroupByIdQuery.UserGroup) -> Unit
+    member: GetGroupByIdQuery.UserGroup, onMemberClicked: (GetGroupByIdQuery.UserGroup) -> Unit
 ) {
-    Card(
-        modifier = Modifier
-            .padding(8.dp)
-            .clickable { onMemberClicked(member) }
-    ) {
+    Card(modifier = Modifier
+        .padding(8.dp)
+        .clickable { onMemberClicked(member) }) {
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -394,3 +435,7 @@ fun GroupMemberCard(
         }
     }
 }
+
+data class TabItem(
+    val title: String, val unselectedIcon: ImageVector, val selectedIcon: ImageVector
+)
